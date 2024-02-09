08de7f159b93dce67fdabf6912a2baba8040774c
Poni03 <130127590+Poni03@users.noreply.github.com>
2024-02-09 15:13:23 +0700

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("join-room").addEventListener("click", () => {
-  var nick = document.getElementById("nickname").value
-  console.log(nick)
-  const roomNameOrKey = document.getElementById("join-room-modal").value;
-  if (roomNameOrKey === "") {
-    alert("Введите название или ключ комнаты");
-    return;
-    }
-});
